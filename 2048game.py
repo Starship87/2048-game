43b55639cfddbe70c253a6794239eb20edccf863
Starship87 <55468638+Starship87@users.noreply.github.com>
2020-01-21 19:32:58 -0600

    Add files via upload

--- 2048game.py
+++ 2048game.py
@@ -1,10 +1,28 @@
 #2048 game
 import random
+from  tkinter import *
+
+root = Tk()
 
 score = 0
 highscore = 0
 
 board = []
+squares = []
+colors={
+    0: "lime",
+    2: "purple",
+    4: "aqua",
+    8: "BlanchedAlmond",
+    16:"Crimson",
+    32:"blue",
+    64:"cyan",
+    128:"yellow",
+    256:"coral",
+    512:"green",
+    1024:"pink",
+    2048:"violet",
+    }
 
 def newgame():
     global board
@@ -18,12 +36,48 @@ def newgame():
     newnumber()
     newnumber()
 
+def makeboard():
+    global squares
+    #set up tkinter controls
+    #title
+    title = Label(root, text="Awesome 2048 Game",font="Arial 17 bold")
+    title.grid(row=0, column=0, columnspan=4)
+    #instructions
+    instruc = Label(root, text="use the arrow keys to move",font="arial 12")
+    instruc.grid(row=1, column=0, columnspan=4)
+    #restart or new game
+    restart = Button(root, text="New Game", font="Arial 14 bold")
+    restart.grid(row=6, column=0, columnspan=2)
+    #quit
+    quitbtn = Button(root, text="Quit", font="Arial 14 bold")
+    quitbtn.grid(row=6, column=2, columnspan=2)
+    #all the squares
+    for r in range(4):
+        temprow = []
+        for c in range(4):
+            templabel = Label(root, height=2, width=4, relief="ridge", font="Arial 16 bold")
+            temprow.append(templabel)
+        squares.append(temprow)
+        
+    for r in range(4):
+        for c in range(4):
+            sq = squares[r][c]
+            sq.grid(row=r+2, column=c)
+
 def showboard():
     for i in range(4):
         row = ""
         for j in range(4):
             row = row + str(board[i][j]) + " "
         print(row)
+
+def displayboard():
+    global squares
+    for r in range(4):
+        for c in range(4):
+            mycolor = colors[board[r][c]]
+            squares[r][c].config(text=board[r][c],bg=mycolor)
+        
 
 def newnumber():
     newnum = 0
@@ -62,6 +116,33 @@ def move():
     newnumber()
     showboard()
     move()
+
+def moveleft(e):
+    rotate(2)
+    swiperight()
+    rotate(2)
+    displayboard()
+def moveright(e):
+    swiperight()
+    displayboard()
+def moveup(e):
+    rotate(1)
+    swiperight()
+    rotate(3)
+    displayboard()
+def movedown(e):
+    rotate(3)
+    swiperight()
+    rotate(1)
+    displayboard()
+
+def bindkeys():
+    global root
+    root.bind("<down>", movedown)
+    root.bind("<up>", moveup)
+    root.bind("<right>", moveright)
+    root.bind("<left>", moveleft)
+    
 
 def swiperight():
     #a function to add the numbers and move them right
@@ -106,6 +187,8 @@ def rotate(number_turns):
                     if item == 2048:
                         win = True
     return win
+makeboard()
 newgame()
-showboard()
-move()
+displayboard()
+bindkeys()
+root.mainloop()
