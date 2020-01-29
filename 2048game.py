ade141ac093448d0192960a5f37ae236bd4c33ca
Starship87 <55468638+Starship87@users.noreply.github.com>
2020-01-28 19:02:48 -0600

    Add files via upload

--- 2048game.py
+++ 2048game.py
@@ -8,11 +8,12 @@ score = 0
 highscore = 0
 
 board = []
+lastboard = []
 squares = []
 colors={
     0: "lime",
     2: "purple",
-    4: "aqua",
+    4: "magenta",
     8: "BlanchedAlmond",
     16:"Crimson",
     32:"blue",
@@ -36,6 +37,12 @@ def newgame():
     newnumber()
     newnumber()
 
+def restartgame():
+    newgame()
+    displayboard()
+def quitnow():
+    root.destroy()
+
 def makeboard():
     global squares
     #set up tkinter controls
@@ -46,10 +53,10 @@ def makeboard():
     instruc = Label(root, text="use the arrow keys to move",font="arial 12")
     instruc.grid(row=1, column=0, columnspan=4)
     #restart or new game
-    restart = Button(root, text="New Game", font="Arial 14 bold")
+    restart = Button(root, text="New Game", font="Arial 14 bold", command=restartgame)
     restart.grid(row=6, column=0, columnspan=2)
     #quit
-    quitbtn = Button(root, text="Quit", font="Arial 14 bold")
+    quitbtn = Button(root, text="Quit", font="Arial 14 bold", command=quitnow)
     quitbtn.grid(row=6, column=2, columnspan=2)
     #all the squares
     for r in range(4):
@@ -80,18 +87,19 @@ def displayboard():
         
 
 def newnumber():
-    newnum = 0
-    newrow = random.randint(0,3)
-    newcol = random.randint(0,3)
-    while board[newrow][newcol] != 0:
+    if checkplayable():
+        newnum = 0
         newrow = random.randint(0,3)
         newcol = random.randint(0,3)
-    rand = random.randint(1,100)
-    if rand < 80:
-        newnum = 2
-    else:
-        newnum = 4
-    board[newrow][newcol] = newnum
+        while board[newrow][newcol] != 0:
+            newrow = random.randint(0,3)
+            newcol = random.randint(0,3)
+        rand = random.randint(1,100)
+        if rand < 80:
+            newnum = 2
+        else:
+            newnum = 4
+        board[newrow][newcol] = newnum
 
 def move():
     print("Select your direction (<, >, v, ^)")
@@ -118,30 +126,66 @@ def move():
     move()
 
 def moveleft(e):
+    copyboard()
     rotate(2)
     swiperight()
     rotate(2)
+    if checkwin():
+        #do the win stuff code
+        winlabel = Label(root,text="YOU WIN!!", font="Arial 24 bold")
+        winlabel.grid(row=7, column=0, columnspan=4)
+    else:
+        if sameboard(board, lastboard) == False:
+            newnumber()
     displayboard()
+    
 def moveright(e):
+    copyboard()
     swiperight()
+    if checkwin():
+        #do the win stuff code
+        winlabel = Label(root,text="YOU WIN!!", font="Arial 24 bold")
+        winlabel.grid(row=7, column=0, columnspan=4)
+    else:
+        if sameboard(board, lastboard) == False:
+            newnumber()
     displayboard()
+    
 def moveup(e):
+    copyboard()
     rotate(1)
     swiperight()
     rotate(3)
+    if checkwin():
+        #do the win stuff code
+        winlabel = Label(root,text="YOU WIN!!", font="Arial 24 bold")
+        winlabel.grid(row=7, column=0, columnspan=4)
+    else:
+        if sameboard(board, lastboard) == False:
+            newnumber()
     displayboard()
+  
 def movedown(e):
+    copyboard()
     rotate(3)
     swiperight()
     rotate(1)
+    if checkwin():
+        #do the win stuff code
+        winlabel = Label(root,text="YOU WIN!!", font="Arial 24 bold")
+        winlabel.grid(row=7, column=0, columnspan=4)
+    else:
+        if sameboard(board, lastboard) == False:
+            newnumber()
     displayboard()
+
 
 def bindkeys():
     global root
-    root.bind("<down>", movedown)
-    root.bind("<up>", moveup)
-    root.bind("<right>", moveright)
-    root.bind("<left>", moveleft)
+    root.bind("<Down>", movedown)
+    root.bind("<Up>", moveup)
+    root.bind("<Right>", moveright)
+    root.bind("<Left>", moveleft)
     
 
 def swiperight():
@@ -180,13 +224,41 @@ def rotate(number_turns):
         board.append(temp2)
         board.append(temp3)
         board.append(temp4)
-        def checkwin():
-            win = False
-            for row in board:
-                for item in row:
-                    if item == 2048:
-                        win = True
+def checkwin():
+    win = False
+    for row in board:
+        for item in row:
+            if item == 2048:
+                win = True
     return win
+
+def checkplayable():
+    #this function checks for an open spot to place a new number
+    playable = False
+    for row in board:
+        for item in row:
+            if item == 0:
+                playable = True
+    return playable
+
+def sameboard(b1, b2):
+    same = True
+    for i in range(4):
+        for j in range(4):
+            if b1[i][j] != b2[i][j]:
+                same = False
+    return same
+
+def copyboard():
+    global board
+    global lastboard
+    lastboard = []
+    
+    for row in board:
+        temprow = row.copy()
+        lastboard.append(temprow)
+        
+    
 makeboard()
 newgame()
 displayboard()
